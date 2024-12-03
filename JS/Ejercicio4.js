cc04d0159348b04d78def20b0f96593c4c23e58f
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2024-12-03 20:11:32 -0300

    se modifico el ejercicio 4

--- JS/Ejercicio4.js
+++ JS/Ejercicio4.js
@@ -1,13 +1,28 @@
-let numero = "" ;
-let total = 0;
-do {
-    numero = parseFloat(prompt("Ingrese un número"));
-    if (isNaN(numero)) {
-        alert("No ingresó un numero válido")
-    }else{
-        total += numero;
-    }
+// let numero = "" ;
+// let total = 0;
+// do {
+//     numero = parseFloat(prompt("Ingrese un número"));
+//     if (isNaN(numero)) {
+//         alert("No ingresó un numero válido")
+//     }else{
+//         total += numero;
+//     }
     
-} while (confirm("Desea continuar añadiendo números?"));
+// } while (confirm("Desea continuar añadiendo números?"));
 
-document.write(`La suma total de los numeros introducidos es: ${total}`);
+// document.write(`La suma total de los numeros introducidos es: ${total}`);
+
+let suma = 0;
+do {
+    let numero = prompt('Ingrese un numero')
+    if (numero === null) {
+        break;
+        
+    }else if (!isNaN(numero) && numero.trim() !== '') {
+        suma += +(numero)
+    }else{
+        alert(`${numero} no es un numero valido`)
+    }
+} while (true);
+
+document.write('La suma de los numero es: ' +suma)
