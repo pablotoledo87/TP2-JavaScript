6dd797761898369fa38206772676ed91b9644b6a
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2024-12-03 20:02:55 -0300

    se modifico el ejercicio 3

--- JS/Ejercicio3.js
+++ JS/Ejercicio3.js
@@ -1,13 +1,27 @@
-let union = "";
-do {
-  let cadena = prompt("Ingrese una cadena de texto");
-  if (cadena !== null && cadena !== "") {
-    union += cadena + " - ";
+// let union = "";
+// do {
+//   let cadena = prompt("Ingrese una cadena de texto");
+//   if (cadena !== null && cadena !== "") {
+//     union += cadena + " - ";
+//   }
+// } while (confirm("Desea agregar mas texto?"));
+// union = union.substring(0,union.length-3)
+// if (union === "") {
+//   document.write(`No ingresó ningun texto`);
+// } else {
+//   document.write(`${union}`);
+// }
+ let resultado = "";
+ do {
+  let palabra = prompt('Ingrese una palabra')
+  if (palabra === null) {
+    alert('Ingrese una palabra')
+    break
+  } else if(resultado === ""){
+    resultado = palabra;
+  }else{
+    resultado = resultado + '-' + palabra
   }
-} while (confirm("Desea agregar mas texto?"));
-union = union.substring(0,union.length-3)
-if (union === "") {
-  document.write(`No ingresó ningun texto`);
-} else {
-  document.write(`${union}`);
-}
+ } while (confirm('Desea continuar?'));
+
+ document.write(resultado || 'No se ingresaron palabras')
